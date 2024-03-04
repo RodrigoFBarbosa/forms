5517f076f69dee232205a3476029c16d1e0503cd
RodrigoFBarbosa <rodrigobarbosa.p99@gmail.com>
2024-03-04 17:22:17 -0300

    training forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Usuario } from './Usuario';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'forms';
+
+  usuarios: Usuario[] = [
+    {name: "Rodrigo", lastName: "Barbosa"},
+    {name: "Lola", lastName: "Alves"},
+    {name: "George", lastName: "Rof"}
+  ]
 }
